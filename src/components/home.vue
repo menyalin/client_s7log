9875fb20d88e4c2d2372458c8f2d41e0c591529d
menyalin <menyalin@icloud.com>
2019-11-04 16:34:08 +0300

    #

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,31 +1,46 @@
 <template>
   <div>
-    <app-document-list-form :items="drivers" :fields="fields"/>
+    <v-autocomplete :loading="$apollo.loading" :search-input.sync="search" hide-no-data />
+    <p>{{ filteredAddresses }}</p>
   </div>
 </template>
 
 <script>
-  import appDocumentListForm from '@/components/common/documentListForm/index.vue'
-
-  export default {
-    data: () => ({
-      drivers: [
-        { _id: '000001', name: 'Иванов Иван Иванович', birthDay: '1990-01-01' },
-        { _id: '000002', name: 'Петров Петр Петрович', birthDay: '1974-04-15' },
-        {
-          _id: '00003233232323232302',
-          name: 'Петров Петр Петрович111111111111111111111111',
-          birthDay: '1974-04-15'
-        }
-      ],
-      fields: [
-        { data: '_id', title: 'ID', cols: '3' },
-        { data: 'name', title: 'NAME', cols: '8' },
-        { data: 'birthDay', title: 'Birth Day', cols: '1' }
-      ]
-    }),
-    components: {
-      appDocumentListForm
+import gql from 'graphql-tag'
+const query = gql`
+  query filteredAddresses($filter: String) {
+    filteredAddresses(filter: $filter) {
+      _id
+      shortName
+      address
+      partner
     }
   }
+`
+export default {
+  data: () => ({
+    filteredAddresses: [],
+    filter: '',
+    search: null
+  }),
+  watch: {
+    search: function(val) {
+      this.filter = val
+      console.log(this.filter)
+    }
+  },
+  apollo: {
+    filteredAddresses: {
+      query,
+      variables() {
+        return {
+          filter: this.filter
+        }
+      },
+      skip() {
+        return this.filter.length < 2
+      }
+    }
+  }
+}
 </script>
