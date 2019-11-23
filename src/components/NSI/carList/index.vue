4b4b489235a40ed842e3388c48842df68fc17849
Menyalin Alexey <menyalin@icloud.com>
2019-11-23 22:20:10 +0300

    #

--- src/components/NSI/carList/index.vue
+++ src/components/NSI/carList/index.vue
@@ -2,7 +2,19 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <v-data-table class="elevation-1" hide-default-footer dense></v-data-table>
+        {{ carPage.totalCar }}
+        <v-data-table
+          class="elevation-1"
+          hide-default-footer
+          :items="carPage.cars"
+          :items-per-page="limit"
+          :headers="headers"
+          dense
+        >
+          <template v-slot:item.isOwned="{ item }">
+            <v-icon v-if="item.isOwned" small color="green">mdi-check</v-icon>
+          </template>
+        </v-data-table>
         <div class="text-center">
           <v-pagination
             v-model="page"
@@ -17,20 +29,85 @@
 
 <script>
 import gql from 'graphql-tag'
+const carPageQuery = gql`
+  query carPage($limit: Int!, $offset: Int!) {
+    carPage(limit: $limit, offset: $offset) {
+      cars {
+        id
+        title
+        reg
+        pts
+        isOwned
+        type
+        maxPltCount
+        note
+      }
+      totalCar
+    }
+  }
+`
 
 export default {
   data: () => ({
     page: 1,
-    limit: 30,
-    carPage: {}
+    limit: 50,
+    carPage: {},
+    headers: [
+      {
+        text: 'Собственная',
+        value: 'isOwned',
+        align: 'center',
+        sortable: false,
+        width: '6em'
+      },
+      {
+        text: 'Заголовок',
+        value: 'title',
+        align: 'left',
+        sortable: false,
+        width: '8em'
+      },
+      {
+        text: 'Тип',
+        value: 'type',
+        align: 'left',
+        sortable: false,
+        width: '8em'
+      },
+      {
+        text: 'Гос.номер',
+        value: 'reg',
+        align: 'left',
+        sortable: false,
+        width: '8em'
+      },
+      {
+        text: 'ПТС',
+        value: 'pts',
+        align: 'left',
+        sortable: false,
+        width: '8em'
+      },
+      {
+        text: 'Примечание',
+        value: 'note',
+        align: 'left',
+        sortable: false
+      }
+    ]
   }),
   apollo: {
     carPage: {
-      query: 
+      query: carPageQuery,
+      variables() {
+        return {
+          limit: this.limit,
+          offset: (this.page - 1) * this.limit
+        }
+      }
     }
   }
 }
 </script>
 
-<style>
-</style>
+<style></style>
