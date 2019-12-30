2d0481f2bec2a9f2984df42cda0aa2a45216f861
Menyalin Alexey <menyalin@icloud.com>
2019-12-30 20:45:06 +0300

    перед переделкой вывода заказов

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,20 +1,12 @@
 <template>
-  <div>
-    {{
-      carWorkScheduleFiltered(
-        '56560de5-e56a-44f2-a53e-f0ce270ad38d',
-        '2019-12-26',
-        '01'
-      )
-    }}
-  </div>
+  <div></div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 export default {
   computed: {
-    ...mapGetters(['carWorkScheduleCells', 'carWorkScheduleFiltered'])
+    ...mapGetters([])
   }
 }
 </script>
