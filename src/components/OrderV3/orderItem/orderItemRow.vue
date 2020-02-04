d5ed8c591f954901c088fae9f1a3bbfe4dc51fea
Menyalin Alexey <menyalin@icloud.com>
2020-02-04 19:07:16 +0300

    #

--- src/components/OrderV3/orderItem/orderItemRow.vue
+++ src/components/OrderV3/orderItem/orderItemRow.vue
@@ -28,9 +28,9 @@ export default {
     ...mapGetters(['isAddressesUpload', 'addressById']),
     tooltipText() {
       if (this.partnerId && this.isAddressesUpload)
-        return `${this.addressById(this.partnerId).shortName} - ${
-          this.addressById(this.partnerId).address
-        }`
+        return `${this.addressById(this.partnerId).partner} - ${
+          this.addressById(this.partnerId).shortName
+        } - ${this.addressById(this.partnerId).address}`
       else return null
     }
   }
